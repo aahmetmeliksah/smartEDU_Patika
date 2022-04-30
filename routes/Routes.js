7563994366e5f5b8b39805d59a2c0d0d4a4baceb
unknown <aahmetmeliksah@gmail.com>
2022-05-01 00:40:21 +0300

    ejs added

--- routes/Routes.js
+++ routes/Routes.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router1 = express.Router();
 
 router1.get("/", (req, res) => {
-  res.status(200).render("index");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
 });
 
 router1.get("/about", (req, res) => {
-    res.status(200).render("about")
-})
+  res.status(200).render("about", {
+    page_name: "about",
+  });
+});
 
 module.exports = router1;
